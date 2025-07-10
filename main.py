ac9e69ff24b1d05ef40e2b491f3972acc6740a94
suhail2341 <85217070+suhail2341@users.noreply.github.com>
2025-07-10 15:21:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,5 +15,4 @@ async def health_check():
     return {"status": "ok"}
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080)) 
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
